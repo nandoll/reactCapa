ed9787e4f5ba2530a1e3145633da7727ea84c52e
Fernando Antezana <fernando.antezana@hundred.com.pe>
2020-10-10 16:49:19 -0500

    Card con imagen

---
+++ 01-semana/src/components/ui/Card.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const Card = ({ title, subTitle}) => {
+  
+  return (
+    <div className="cardContainer">
+      <div className="card">
+        <div className="cardUserImage">
+          <img src="./assets/avatar.jpg" alt="Juanito Perez"/>          
+        </div>
+        <div className="cardTitle">
+          <h2> { title } </h2>
+        </div>
+        <div className="cardSubTitle">
+          <p> { subTitle } </p>
+        </div>
+      </div>
+    </div>
+  )
+}
